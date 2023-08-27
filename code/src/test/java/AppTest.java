365cd4dd8d9af17268911e573c86b387edb5057f
autores-uk <autores.uk@protonmail.me>
2023-08-27 16:36:54 +0100

    Hello World

---
+++ code/src/test/java/AppTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
