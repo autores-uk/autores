95cf1194bf5ddc2fbe6c2bd7b16e20c9e0f48c6e
autores-uk <autores.uk@protonmail.me>
2023-09-29 11:41:37 +0100

    Releasing version alpha-0.0.0

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,4 +6,40 @@ BASE=$(dirname $0)/..
 
 cd "${BASE}"
 
+# if [[ -z $(git status -s) ]]; then
+#   echo "No uncommitted changes"
+# else
+#   echo "Uncommitted changes; resolve before continuing"
+#   exit 1
+# fi
+
+HERE="$(pwd)"
+MAJOR="$(cat ${HERE}/scripts/versions/major.txt)"
+MINOR="$(cat ${HERE}/scripts/versions/minor.txt)"
+NEXTMINOR="$((${MINOR}+1))"
+
+CURRENT="${MAJOR}.${MINOR}"
+NEXT="${MAJOR}.${NEXTMINOR}"
+
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+RELEASEBRANCH="release/${CURRENT}"
+
+echo "Releasing version ${CURRENT} on branch ${BRANCH}"
+
+mvn --file "${BASE}/code/pom.xml" versions:set -DnewVersion="${NEXT}"
+
+git add code
+git commit -m "Releasing version ${CURRENT}"
+git push origin "${BRANCH}"
+git checkout -b "${RELEASEBRANCH}"
+git push origin "${RELEASEBRANCH}"
+git checkout "${BRANCH}"
+
 mvn --file "${BASE}/code/annotations/pom.xml" clean install -P release
+
+echo "${NEXTMINOR}" > "${HERE}/scripts/versions/minor.txt"
+mvn --file "${BASE}/code/pom.xml" versions:set -DnewVersion="${NEXT}-SNAPSHOT"
+git add code
+git add scripts/versions
+git commit -m "Setting version to ${NEXT}-SNAPSHOT"
+git push origin "${BRANCH}"
