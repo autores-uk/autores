1827ae3ed5ec4eb41fd93ce3781e9d010ba8b396
autores-uk <autores.uk@protonmail.me>
2023-10-06 15:42:21 +0100

    Better module encapsulation

--- code/annotations/src/main/java/module-info.java
+++ code/annotations/src/main/java/module-info.java
@@ -1,11 +1,12 @@
 module uk.autores {
+    // provides annotation processing library
     requires transitive java.compiler;
-
+    // public packages
     exports uk.autores;
     exports uk.autores.processing;
-    exports uk.autores.processors;
-
+    // testable packages
+    exports uk.autores.processors to uk.autores.test;
     exports uk.autores.internal to uk.autores.test;
-
+    // annotation processor
     provides javax.annotation.processing.Processor with uk.autores.processors.ClasspathResourceProcessor;
 }
