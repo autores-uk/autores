204a6bffce1ee65ed8cb0807cedb0799f916518b
autores-uk <autores.uk@protonmail.me>
2023-11-18 17:12:24 +0000

    Better example

---
+++ code/examples/transform-properties-to-constants/src/main/java/uk/autores/consts/PrintCincoLobitos.java
@@ -0,0 +1,46 @@
+package uk.autores.consts;
+
+import uk.autores.GenerateConstantsFromProperties;
+import uk.autores.ResourceFiles;
+
+import java.io.PrintStream;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+@ResourceFiles(
+        value = "CincoLobitos.properties",
+        handler = GenerateConstantsFromProperties.class
+)
+public class PrintCincoLobitos {
+
+    public static String[] lines() {
+        // Constants generated from the properties file keys
+        String[] lines = {
+                CincoLobitos.she_wolf,
+                CincoLobitos.wolf_cubs,
+                CincoLobitos.she_raised,
+                CincoLobitos.and_all,
+                CincoLobitos.she_nursed,
+        };
+        return lines;
+    }
+
+    public static void main(String...args)  {
+        Locale[] locales = {
+                Locale.ROOT,
+                Locale.ENGLISH,
+        };
+
+        print(locales, lines(), System.out);
+    }
+
+    public static void print(Locale[] locales, String[] lines, PrintStream out) {
+        for (int i = 0; i < lines.length; i++) {
+            for (Locale locale : locales) {
+                ResourceBundle bundle = ResourceBundle.getBundle("uk.autores.consts.CincoLobitos", locale);
+                String line = bundle.getString(lines[i]);
+                out.println(i + ": " + line);
+            }
+        }
+    }
+}
