e591a8769a946d8c6b15dd64919e46530d2237d7
autores-uk <autores.uk@protonmail.me>
2023-11-22 16:50:13 +0000

    Documentation

--- code/examples/create-checked-resource/src/main/java/module-info.java
+++ code/examples/create-checked-resource/src/main/java/module-info.java
@@ -1,3 +1,6 @@
+// Copyright 2023 https://github.com/autores-uk/autores/blob/main/LICENSE.txt
+// SPDX-License-Identifier: Apache-2.0
+
 module uk.autores.checked {
     requires static uk.autores;
     requires com.google.common;
--- code/examples/embed-resources-as-bytes/src/main/java/module-info.java
+++ code/examples/embed-resources-as-bytes/src/main/java/module-info.java
@@ -1,3 +1,6 @@
+// Copyright 2023 https://github.com/autores-uk/autores/blob/main/LICENSE.txt
+// SPDX-License-Identifier: Apache-2.0
+
 module uk.autores.bytes {
     requires static uk.autores;
     requires java.xml;
