52eb4e3e99c5d592df229fb5be02361e72b1ee46
autores-uk <autores.uk@protonmail.me>
2024-05-13 17:19:36 +0100

    Minor enhancements

--- code/examples/embed-resources-as-strings/src/test/java/uk/autores/strings/test/PrintRhymesTest.java
+++ code/examples/embed-resources-as-strings/src/test/java/uk/autores/strings/test/PrintRhymesTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PrintRhymesTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void main() {
         PrintRhymes.main();
     }
