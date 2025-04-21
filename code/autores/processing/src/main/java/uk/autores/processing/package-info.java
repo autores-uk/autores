2696b8d46dddc718694ca43ee98f25f4d1d2eb88
autores-uk <autores.uk@protonmail.me>
2025-04-21 16:56:47 +0100

    Documentation

--- code/autores/processing/src/main/java/uk/autores/processing/package-info.java
+++ code/autores/processing/src/main/java/uk/autores/processing/package-info.java
@@ -1,2 +1,13 @@
-/** Annotation processing package - API consumers can ignore. */
+/**
+ * <p>
+ *     Annotation processing package.
+ * </p>
+ * <p>
+ *     This library must be exposed to the compiler's annotation processor
+ *     in order to have annotations defined in the API library processed.
+ *     With the
+ *     <a href="https://dev.java/learn/jvm/tools/core/javac/">javac</a>
+ *     compiler this can be done with the <code>-processorpath</code> flag.
+ * </p>
+ */
 package uk.autores.processing;
