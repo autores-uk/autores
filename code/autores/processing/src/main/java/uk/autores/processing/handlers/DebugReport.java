e5d9f4d2c26fc5d28e33c3cafe8789cc7e7374bb
autores-uk <autores.uk@protonmail.me>
2025-05-06 17:31:31 +0100

    Debug tool

---
+++ code/autores/processing/src/main/java/uk/autores/processing/handlers/DebugReport.java
@@ -0,0 +1,65 @@
+package uk.autores.processing.handlers;
+
+import uk.autores.handling.Context;
+import uk.autores.handling.Handler;
+
+import javax.annotation.processing.Filer;
+import javax.tools.FileObject;
+import javax.tools.JavaFileManager;
+import javax.tools.StandardLocation;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Internal tool for reporting on {@link Filer#getResource(JavaFileManager.Location, CharSequence, CharSequence)}
+ * implementations.
+ */
+public final class DebugReport implements Handler {
+    private final AtomicBoolean done = new AtomicBoolean(false);
+
+    @Override
+    public void handle(Context context) throws Exception {
+        if (done.getAndSet(true)) {
+            return;
+        }
+        String report = document(context);
+        write(context, report);
+    }
+
+    private String document(Context context) {
+        StringBuilder buf = new StringBuilder();
+        for (StandardLocation l : StandardLocation.values()) {
+            location(context, l, buf);
+        }
+        return buf.toString();
+    }
+
+    private void location(Context context, JavaFileManager.Location l, StringBuilder buf) {
+        if (context.resources().isEmpty()) {
+            return;
+        }
+        CharSequence r = context.resources().get(0);
+        Filer filer = context.env().getFiler();
+        try {
+            filer.getResource(l, context.pkg(), r);
+            buf.append(l).append(" OK\n");
+        } catch (Exception e) {
+            buf.append(l).append(" ").append(e.toString()).append("\n");
+        }
+    }
+
+    private void write(Context context, String results) {
+        Filer filer = context.env().getFiler();
+        try {
+            String name = DebugReport.class.getSimpleName() + "-" + System.identityHashCode(this) + ".txt";
+            FileObject r = filer.createResource(StandardLocation.CLASS_OUTPUT, "", name);
+            try (OutputStream os = r.openOutputStream();
+                 Writer w = new OutputStreamWriter(os, StandardCharsets.UTF_8)) {
+                w.write(results);
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+}
