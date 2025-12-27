c42459cb704a839633fd384b997f2ba1dd9dc789
autores-uk <autores.uk@protonmail.me>
2025-12-27 13:59:39 +0000

    Removed reduntant param

--- code/autores/annotations/src/main/java/uk/autores/ResourceFiles.java
+++ code/autores/annotations/src/main/java/uk/autores/ResourceFiles.java
@@ -66,7 +66,7 @@ public @interface ResourceFiles {
      *
      * @return processing instructions
      */
-    Processing processing() default @Processing(namer = Namer.class);
+    Processing processing() default @Processing();
 
     /**
      * Some implementations of {@link Handler} support configuration.
